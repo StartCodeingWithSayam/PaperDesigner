467d74b3d71e9db9242a861746f49940c3cd3f02
Sayam sharma <92935635+StartCodeingWithSayam@users.noreply.github.com>
2022-10-11 20:11:07 +0530

    Final Commit

--- app/src/main/java/com/sayam/papperdesigners/MainSelector.java
+++ app/src/main/java/com/sayam/papperdesigners/MainSelector.java
@@ -1,18 +1,14 @@
 package com.sayam.papperdesigners;
 
-import static com.sayam.papperdesigners.R.id.school;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 
 public class MainSelector extends AppCompatActivity {
 
     AppCompatButton school,organization;
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +18,10 @@ public class MainSelector extends AppCompatActivity {
             startActivity(new Intent(this,SelectorActivity.class));
             finish();
         });
-        findViewById(R.id.organization).setOnClickListener(view -> {
+        organization = findViewById(R.id.organization);
+        organization.setOnClickListener(view -> {
             startActivity(new Intent(this, ClientPageActivity.class));
             finish();
         });
-
     }
 }
