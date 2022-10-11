3e4d78553c320022b6acaab5a28f6b2e8f166f57
Sayam sharma <92935635+StartCodeingWithSayam@users.noreply.github.com>
2022-10-11 17:56:58 +0530

    Final Commit

--- app/src/main/java/com/sayam/papperdesigners/BusinessLoginPageActivity.java
+++ app/src/main/java/com/sayam/papperdesigners/BusinessLoginPageActivity.java
@@ -28,12 +28,12 @@ import java.util.Objects;
 
 public class BusinessLoginPageActivity extends AppCompatActivity {
     private static final int RC_SIGN_IN = 23;
-    EditText email,password;
-    TextView makeAccount,reset;
-    Button login,google;
-    GoogleSignInClient client;
-    FirebaseAuth auth;
-    FirebaseDatabase database;
+    EditText email_bs,password_bs;
+    TextView makeAccount_bs,reset_bs;
+    Button login_bs,google_bs;
+    GoogleSignInClient client_bs;
+    FirebaseAuth mAuth;
+    FirebaseDatabase database_bs;
     ProgressDialog dialog;
 
 
@@ -44,32 +44,32 @@ public class BusinessLoginPageActivity extends AppCompatActivity {
         getActionBar().hide();
         init();
         createRequest();
-        makeAccount.setOnClickListener(view -> {
+        makeAccount_bs.setOnClickListener(view -> {
             startActivity(new Intent(this,SignInActivity.class));
             finish();
         });
-        login.setOnClickListener(v->{
+        login_bs.setOnClickListener(v->{
             dialog.show();
             LogInWithEmailPassword();
         });
-        google.setOnClickListener(v-> signIn());
-        reset.setOnClickListener(v->startActivity(new Intent(this,ForgotPassword.class)));
+        google_bs.setOnClickListener(v-> signIn());
+        reset_bs.setOnClickListener(v->startActivity(new Intent(this,ForgotPassword.class)));
     }
 
     private void LogInWithEmailPassword(){
-        auth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+        mAuth.signInWithEmailAndPassword(email_bs.getText().toString(),password_bs.getText().toString())
                 .addOnCompleteListener(task -> {
                     dialog.dismiss();
                     if (!task.isSuccessful()){
                         Toast.makeText(this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
                     }else{
-                        FirebaseUser users = auth.getCurrentUser();
+                        FirebaseUser users = mAuth.getCurrentUser();
                         UserModel userModel = new UserModel();
                         assert users != null;
                         userModel.setUserId(users.getUid());
                         userModel.setUserName(users.getDisplayName());
                         userModel.setUserEmail(users.getEmail());
-                        database.getReference().child("Organization")
+                        database_bs.getReference().child("Organization")
                                 .child(users.getUid()).setValue(userModel);
                         startActivity(new Intent(this, MainActivity.class));
                         finish();
@@ -92,16 +92,16 @@ public class BusinessLoginPageActivity extends AppCompatActivity {
 
     private void firebaseAuthWithGoogle(@NonNull GoogleSignInAccount account) {
         AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
-        auth.signInWithCredential(credential)
+        mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(task-> {
                     if (task.isSuccessful()) {
-                        FirebaseUser users = auth.getCurrentUser();
+                        FirebaseUser users = mAuth.getCurrentUser();
                         UserModel userModel = new UserModel();
                         assert users != null;
                         userModel.setUserId(users.getUid());
                         userModel.setUserName(users.getDisplayName());
                         userModel.setUserEmail(users.getEmail());
-                        database.getReference().child("Organization")
+                        database_bs.getReference().child("Organization")
                                 .child(users.getUid()).setValue(userModel);
                         startActivity(new Intent(this, MainActivity.class));
                         finish();
@@ -111,7 +111,7 @@ public class BusinessLoginPageActivity extends AppCompatActivity {
     }
     @SuppressWarnings("deprecation")
     private void signIn(){
-        Intent i = client.getSignInIntent();
+        Intent i = client_bs.getSignInIntent();
         startActivityForResult(i,RC_SIGN_IN);
     }
     private void createRequest(){
@@ -119,17 +119,17 @@ public class BusinessLoginPageActivity extends AppCompatActivity {
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
-        client = GoogleSignIn.getClient(this,gso);
+        client_bs = GoogleSignIn.getClient(this,gso);
     }
     private void init(){
-        email = findViewById(R.id.et_emailId_bss);
-        password = findViewById(R.id.et_passwordId_bss);
-        makeAccount = findViewById(R.id.make_account_bs);
-        reset = findViewById(R.id.tv_reset_password_bss);
-        login = findViewById(R.id.btn_login_bss);
-        auth  = FirebaseAuth.getInstance();
-        database  = FirebaseDatabase.getInstance();
-        google = findViewById(R.id.google_logIn_bs);
+        email_bs = findViewById(R.id.et_emailId_bss);
+        password_bs = findViewById(R.id.et_passwordId_bss);
+        makeAccount_bs = findViewById(R.id.make_account_bs);
+        reset_bs = findViewById(R.id.tv_reset_password_bss);
+        login_bs = findViewById(R.id.btn_login_bss);
+        mAuth = FirebaseAuth.getInstance();
+        database_bs = FirebaseDatabase.getInstance();
+        google_bs = findViewById(R.id.google_logIn_bs);
         dialog = new ProgressDialog(this);
         dialog.setTitle("LogIn...");
         dialog.setMessage("Logging you in...");
@@ -138,7 +138,7 @@ public class BusinessLoginPageActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        if (auth.getCurrentUser() != null) {
+        if (mAuth.getCurrentUser() != null) {
             startActivity(new Intent(this,MainActivity.class));
         }
     }
